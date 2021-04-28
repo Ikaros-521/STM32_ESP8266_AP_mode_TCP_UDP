fd710f37c43deff1186a5ae8a087471001cadabd
ikaros <327209194@qq.com>
2021-04-28 16:53:14 +0800

    新增分支，自选模式+命令控制（需要DHT11），仅供学习参考

--- USER/main.c
+++ USER/main.c
@@ -16,9 +16,9 @@ void ap_demo(void);
 
 int main(void)
 {
-    delay_init();	    	 //延时函数初始化
+    delay_init();	    	 	//延时函数初始化
     NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);//设置中断优先级分组为组2：2位抢占优先级，2位响应优先级
-    uart_init(115200);	 	//串口初始化为115200
+    uart_init(115200);	 		//串口初始化为115200
     usmart_dev.init(72);		//初始化USMART
     LED_Init();		  			//初始化与LED连接的硬件接口
     KEY_Init();					//初始化按键
@@ -27,20 +27,18 @@ int main(void)
     usart3_init(115200);		//初始化串口3
     my_mem_init(SRAMIN);		//初始化内部内存池
     //LCD_Clear(BLACK);
-	/*
 	while(DHT11_Init())			//DHT11初始化 DATA -> PG11
 	{
 		printf("DHT11 Error\r\n");
 	}
-	*/
 
     delay_ms(1000);
 	
 	BEEP = 0;
 	LED1 = 0;
-	ap_demo();
+	// ap_demo();
 	
-    // atk_8266_test();		//进入ATK_ESP8266测试
+    atk_8266_test();		//进入ATK_ESP8266测试
 }
 
 // AP模式测试
